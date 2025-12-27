9eab75f472407fb180356c29eb21755eefac9c92
shivgez <123392737+shivgez@users.noreply.github.com>
2025-12-28 00:07:22 +0530

    Time: 0 ms (100%), Space: 11.9 MB (46.85%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -7,7 +7,7 @@ public:
     string prefix=strs[0];
     for(int i=1;i<strs.size();i++){
         int j=0;
-        while(j<prefix.size()&&j<strs[i].size()&& prefix[j]==strs[i][j]){
+        while(j<prefix.length()&&j<strs[i].length()&& prefix[j]==strs[i][j]){
             j++;
         }
         prefix=prefix.substr(0,j);
