d95b796f5067c9c521d1a606c1f4a8f1f2a9d75b
shivgez <123392737+shivgez@users.noreply.github.com>
2026-01-31 16:40:18 +0530

    Time: 0 ms (100%), Space: 9 MB (99.81%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char ch=0;
+        for(int i=0;i<s.length();i++){
+            ch^=s[i];
+        }
+  for(int i=0;i<t.length();i++){
+    ch^=t[i];
+  }
+  return ch;
+    
+
+    }
+};
