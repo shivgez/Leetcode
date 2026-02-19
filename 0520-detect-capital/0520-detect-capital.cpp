5d8d73bde0f9ec82ffe4081f6310487a53055ca8
shivgez <123392737+shivgez@users.noreply.github.com>
2026-02-19 18:46:35 +0530

    Time: 0 ms (100%), Space: 8.3 MB (15.99%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -1,44 +1,22 @@
 class Solution {
 public:
-    bool detectCapitalUse(string word) {
-        bool flag=false;
+    bool detectCapitalUse(string word) { 
+        int uppercase=0;
         int n=word.length();
-        int i=0;
-      //  while(i<n){
-        if(word[0]>='A'&& word[0]<='Z' &&   word[1]>='a' &&word[1]<='z'){
-            
-            for(int j=1;j<n;j++){
-                if(word[j]>='a'&&word[j]<='z'){
-                    flag=true;
-                }
-                else{
-                    return false;
-                }
-            }
-        }else{
-
-            if(word[0]>='A' && word[0]<='Z'){
-                
-                for(int j=i;j<n;j++){
-                if(word[j]>='A'&&word[j]<='Z'){
-                    flag=true;
-                }else{
-                return false;
-                }
-                }
-            }
-            if(word[0]>='a' && word[0]<='z'){
-                
-                for(int j=i;j<n;j++){
-                  if(word[j]>='a'&&word[0]<='z'){
-                    flag=true;
-                  }
-                  else{
-                    return false;
-                }
-                }
+        for(char ch:word){
+            if(ch>='A' && ch<='Z'){
+                uppercase++;
             }
         }
-        return flag;
+        if(uppercase==n){
+            return true;
+        }
+        if(uppercase==0){
+            return true;
+        }
+        if(uppercase==1 && word[0]>='A' && word[0]<='Z'){
+            return true;
+        }
+    return false;
     }
 };
