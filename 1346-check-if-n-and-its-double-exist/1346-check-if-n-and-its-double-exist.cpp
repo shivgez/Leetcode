4ecbcd390ce32a398c7122da428388f4434c4901
shivgez <123392737+shivgez@users.noreply.github.com>
2026-02-26 16:02:45 +0530

    Time: 3 ms (27.12%), Space: 13.7 MB (49.3%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            int target=2*arr[i];
+            int low=0,high=n-1;
+            while(low<=high){
+                int mid=low+(high-low)/2;
+                if(mid!=i && arr[mid]==target){
+                    return true;
+                }
+                else if(arr[mid]<target){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
+            }
+
+           }
+           return false;
+    }
+};
