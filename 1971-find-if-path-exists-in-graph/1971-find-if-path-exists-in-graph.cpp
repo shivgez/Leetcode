f62be7ea182fa6374fb64e53b082c25bf7907023
shivgez <123392737+shivgez@users.noreply.github.com>
2026-03-05 22:02:07 +0530

    Time: 385 ms (40.82%), Space: 315.7 MB (36.48%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    vector<int> BFS(int n, vector<vector<int>>& adj, int source){
+    bool BFS(int n, vector<vector<int>>& adj, int source,int destination){
     queue<int> q;
      vector<int> visit(n,0);
      visit[source]=1;
      q.push(source);
-     vector<int> bfs;
+     
      while(!q.empty()){
         
         int node=q.front();
         q.pop();
-        bfs.push_back(node);
+       if(node==destination){ return true;}
         for(auto it: adj[node]){
             if(!visit[it]){
                 visit[it]=1;
@@ -18,7 +18,7 @@ public:
             }
         }
      }
-     return bfs;
+    return false;
     }
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
         vector<vector<int>> adj(n);
@@ -27,10 +27,7 @@ for(auto e : edges){
     adj[e[0]].push_back(e[1]);
     adj[e[1]].push_back(e[0]);
 }
-     vector<int> vec=BFS(n,adj,source);
-     for(auto it: vec){
-        if(it==destination) return true;
-     }
-        return false;
+     return BFS(n,adj,source,destination);
+    
     }
 };
