e78fd346a868e0e0c3253790618b3d51afa1c79d
shivgez <123392737+shivgez@users.noreply.github.com>
2026-03-15 18:11:23 +0530

    Time: 0 ms (100%), Space: 8.5 MB (82.65%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int revnum=0;
+        while(x!=0){
+            int n=x%10;
+            if(revnum>INT_MAX/10 || revnum<INT_MIN/10){
+                return 0;
+            }
+            revnum=(revnum*10) + n;
+            x/=10;
+        }
+        return revnum;
+    }
+};
