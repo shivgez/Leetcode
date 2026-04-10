cb4919be078fadfa7ae03bb5e01b3cfa1f444141
shivgez <123392737+shivgez@users.noreply.github.com>
2026-04-10 19:03:30 +0530

    Time: 8 ms (29.89%), Space: 13.7 MB (89.44%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void dfs(int node, vector<vector<int>>& graph, vector<int>& path, vector<vector<int>>& ans) {
+        
+        path.push_back(node);
+
+        if(node == graph.size() - 1) {
+            ans.push_back(path);
+        } else {
+            for(auto next : graph[node]) {
+                dfs(next, graph, path, ans);
+            }
+        }
+
+        path.pop_back();
+    }
+
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<vector<int>> ans;
+        vector<int> path;
+
+        dfs(0, graph, path, ans);
+
+        return ans;
+    }
+};
